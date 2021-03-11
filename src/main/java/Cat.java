6e5935c8e8dbf86e5805f898f9b88c070bf53cf0
eghbol <eqhbal@gmail.com>
2021-03-11 15:20:48 +0100

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+
+    String naam;
+    String ras;
+}
---
+++ src/main/java/FirstAppT.java
@@ -0,0 +1,16 @@
+public class FirstAppT {
+
+    public static void main(String[] args){
+
+        Cat cat1 = new Cat();
+        cat1.naam = "tim";
+        cat1.ras = "britse korthaaar";
+
+
+        System.out.println("Mijn kaaats naam is " + cat1 + " hij is een " + cat1.ras);
+
+    }
+
+}
+
+
